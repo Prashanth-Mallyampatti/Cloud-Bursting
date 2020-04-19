c527c23646e5a13da34ebc914ff860380c6bdd88
Prashanth-Mallyampatti <prashanth.m1996@gmail.com>
2020-04-19 14:55:09 -0400

    Merging DB script to VCL

--- cloud_bursting/aws_trigger.sh
+++ cloud_bursting/aws_trigger.sh
@@ -4,19 +4,26 @@ INPUT_PARSER="/var/www/html/vcl-2.5.1/cloud_bursting/parse_input.py"
 AWS_ANSIBLE="/var/www/html/vcl-2.5.1/cloud_bursting/ansible/src/create_aws_instance.yml"
 VAULT="/var/www/html/vcl-2.5.1/cloud_bursting/ansible/keys/vault_secret.sh"
 SESSION="/var/www/html/vcl-2.5.1/cloud_bursting/user_data/session.txt"
-LOG_FILE="/var/www/html/vcl-2.5.1/cloud_bursting/ansible/logs/ansible_log"
+LOG_FILE="/var/log/cloud_bursting/logs"
+AWS_OUTPUT="/var/www/html/vcl-2.5.1/cloud_bursting/ansible/var/"
+AWS_DB="/var/www/html/vcl-2.5.1/cloud_bursting/write_table.py"
 
 parse_input()
 {
   if [ -f "$1" ]
   then
-    python3 "$1"
+    echo >> "$LOG_FILE"
+    echo "Parsing user data.." >> "$LOG_FILE"
+    echo >> "$LOG_FILE"
+    python3 "$1" >> "$LOG_FILE"
     if [ $? -ne 0 ]
     then 
-      echo "Parser script execution error"
+      echo "Parser script execution error" >> "$LOG_FILE"
+    else
+      echo "Parsing done!" >> "$LOG_FILE"
     fi
   else
-    echo "File not found"
+    echo "File not found: parse_input.py" >> "$LOG_FILE"
     exit 1
   fi
 }
@@ -25,9 +32,13 @@ fetch_user()
 {
   if [ -f "$1" ]
   then
+    echo >> "$LOG_FILE"
+    echo "Fetching current user name.." >> "$LOG_FILE"
+    echo >> "$LOG_FILE"
     USER_NAME=$(awk -F '@' '{print $1}' $1)
+    echo "Done fetching!" >> "$LOG_FILE"
   else
-    echo "Session file not found"
+    echo "Session file not found: session.txt" >> "$LOG_FILE"
     exit 1
   fi
 }
@@ -36,13 +47,40 @@ aws_call()
 {
   if [[ -f "$1" ]] && [[ -f "$2" ]]
   then
-    ansible-playbook "$1" --extra-vars "user=$3" --vault-password-file "$2" -v >> "$4"
+    echo  >> "$LOG_FILE"
+    echo "Executing playbook.." >> "$LOG_FILE"
+    echo >> "$LOG_FILE"
+    ansible-playbook "$1" --extra-vars "user=$3" --vault-password-file "$2" -v >> "$LOG_FILE"
     if [ $? -ne 0 ]
     then 
-      echo "Ansible playbook execution error"
+      echo "Ansible playbook execution error" >> "$LOG_FILE"
+    else
+      echo "Playbook executed successfully!" >> "$LOG_FILE"
     fi
   else
-    echo "AWS files not found"
+    echo "AWS files not found" >> "$LOG_FILE"
+    exit 1
+  fi
+}
+
+update_aws_db()
+{
+  sleep 5
+  if [[ -f "$1" ]] && [[ -f "$2" ]]
+  then
+    echo >> "$LOG_FILE"
+    echo "Writing to AWS database.." >> "$LOG_FILE"
+    echo >> "$LOG_FILE"
+    python3 "$1" "$2" >> "$LOG_FILE"
+    if [ $? -ne 0 ]
+    then
+      echo "AWS DB script execution error" >> "$LOG_FILE"
+    else
+      echo >> "LOG_FILE"
+      echo "Write successful!" >> "$LOG_FILE"
+    fi
+  else
+    echo "File not found: write_table.py or $2" >> "$LOG_FILE"
     exit 1
   fi
 }
@@ -51,4 +89,6 @@ aws_call()
 
 parse_input "$INPUT_PARSER"
 fetch_user "$SESSION"
-aws_call "$AWS_ANSIBLE" "$VAULT" "$USER_NAME" "$LOG_FILE"
+aws_call "$AWS_ANSIBLE" "$VAULT" "$USER_NAME"
+USER_INSTANCE="$AWS_OUTPUT""$USER_NAME""_instances.yml"
+update_aws_db "$AWS_DB" "$USER_INSTANCE"
