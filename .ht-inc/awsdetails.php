6b71581fbf564be2204e53406554b29bf4d0c409
cperezo <cperezo@ncsu.edu>
2020-04-18 20:53:24 -0400

    wip

--- .ht-inc/awsdetails.php
+++ .ht-inc/awsdetails.php
@@ -7,14 +7,24 @@ function awsdetailsFunc() {
     // for ($i = 0, $i < $count; i++, $text = ''){
 
     // }
+
     $outerText = '';
+
+    $outerText .= "<table>\n";
+    $outerText .= "<tr>\n";
+    $outerText .= "<th>Public IP</th>\n";
+    $outerText .= "<th>Key Name</th>\n";
+    $outerText .= "<th>User</th>\n";
+
     for($i = 0, $noedit = 0, $text = '', $showcreateimage = 0, $cluster = 0, $col3 = 0;
 		   $i < $count;
 		   $i++, $noedit = 0, $text = '', $cluster = 0, $col3 = 0) {
             
-            $text .= "{$requests[$i]['public_ip']}<br>\n";
-            $text .= "{$requests[$i]['key_name']}<br>\n";
-            $text .= "{$requests[$i]['user']}<br>\n";
+            $text .= "<tr>\n";
+            $text .= "<td>{$requests[$i]['public_ip']}</td>\n";
+            $text .= "<td>{$requests[$i]['key_name']}</td>\n";
+            $text .= "<td>{$requests[$i]['user']}</td>\n";
+            $text .= "</tr>\n";
 
             $outerText .= $text;
     };
