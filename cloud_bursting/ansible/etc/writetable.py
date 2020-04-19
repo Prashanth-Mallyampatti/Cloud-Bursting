3134d43ff770dd9e56a414f8e0df291656fce2c9
vsingh7 <vsingh7@ncsu.edu>
2020-04-18 22:39:54 -0400

    Added dns entry to awsuser table in DB
    Delete existing awsuser table before running this script

--- cloud_bursting/ansible/etc/writetable.py
+++ cloud_bursting/ansible/etc/writetable.py
@@ -17,6 +17,7 @@ awsdict['instance_id'] = aws_list['results'][0]['instance_ids'][0]
 awsdict['public_ip'] = aws_list['results'][0]['instances'][0]['public_ip']
 awsdict['key_name'] = aws_list['results'][0]['item']['key_name']
 awsdict['user'] = aws_list['results'][0]['item']['user']
+awsdict['dns'] = aws_list['results'][0]['instances'][0]['dns_name']
 
 try:
   mydb = mysql.connector.connect(
@@ -28,14 +29,11 @@ try:
 
   mycursor.execute("USE vcl;")
 
-  mycursor.execute("CREATE TABLE IF NOT EXISTS awsuser (instance_id VARCHAR(255) PRIMARY KEY, public_ip VARCHAR(255), key_name VARCHAR(255), user VARCHAR(255), private_key TEXT)")
+  mycursor.execute("CREATE TABLE IF NOT EXISTS awsuser (instance_id VARCHAR(255) PRIMARY KEY, public_ip VARCHAR(255), key_name VARCHAR(255), user VARCHAR(255), private_key VARCHAR(255), dns VARCHAR(255))")
 
-  values = (awsdict['instance_id'], awsdict['public_ip'], awsdict['key_name'], awsdict['user'])
-  print("createtable: command run - INSERT IGNORE INTO awsuser (instance_id, public_ip, key_name, user, private_key) VALUES (%s,%s,%s,%s,%s)",values)
-  mycursor.execute("INSERT IGNORE INTO awsuser (instance_id, public_ip, key_name, user) VALUES (%s,%s,%s,%s)",values)
-  
-  values = (awsdict['key'], awsdict['instance_id'])
-  mycursor.execute("UPDATE awsuser SET private_key = AES_ENCRYPT(%s, 'test') WHERE instance_id = %s", values)
+  values = (awsdict['instance_id'], awsdict['public_ip'], awsdict['key_name'], awsdict['user'], awsdict['private_key'], awsdict['dns'])
+  print("createtable: command run - INSERT IGNORE INTO awsuser (instance_id, public_ip, key_name, user, private_key, dns) VALUES (%s,%s,%s,%s,%s,%s)",values)
+  mycursor.execute("INSERT IGNORE INTO awsuser (instance_id, public_ip, key_name, user, private_key, dns) VALUES (%s,%s,%s,%s,%s,%s)",values)
   mydb.commit()
 except error:
   print("daed: ")
